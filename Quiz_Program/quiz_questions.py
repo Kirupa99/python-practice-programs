f1e96130fa0ad5d32b57330b2080cc29adb62c8b
KirupaVasini <kirupavasini25@gmail.com>
2025-04-30 12:40:24 +0100

    Create quiz_questions.py

---
+++ Quiz_Program/quiz_questions.py
@@ -0,0 +1,4 @@
+Easy_Questions={"What is 2+3 is equal to?":"5","What is the name of the animal that barks?:":"dog","What is the opposite for happy?":"sad","What is the colour of a crow?":"black"}
+Medium_Questions={"What is the process by which plants convert sunlight to energy?":"photosynthesis","What is the main ingredient in hummus?":"chickpea","What meat is used in a shepherd's pie?":"lamb","What is the national sport of Japan?":"sumo wrestling"}
+Hard_Questions={"True or false: Holland is a region in The Netherlands?":"true","Which country drinks the most coffee per capita?":"finland","In which country was Elon Musk born?":"south africa","What is the 4th letter of the Greek alphabet?":"delta"}
+
