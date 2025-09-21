a23346290410e3d95e067bdae7971d638578d826
KirupaVasini <kirupavasini25@gmail.com>
2025-09-21 14:32:22 +0100

    Create compare_data.py

---
+++ HigherLower/compare_data.py
@@ -0,0 +1,92 @@
+sample_influencers = [
+    {
+        "name": "Virat Kohli",
+        "description": "Cricket player, sports star",
+        "country": "India",
+        "followers": 270000000
+    },
+    {
+        "name": "Lisa (BLACKPINK)",
+        "description": "K-pop idol, dancer, fashion icon",
+        "country": "Thailand",
+        "followers": 99700000
+    },
+    {
+        "name": "Ariana Grande",
+        "description": "Singer, actress",
+        "country": "United States",
+        "followers": 374000000
+    },
+    {
+        "name": "Nicki Minaj",
+        "description": "Rapper, singer, cultural influencer",
+        "country": "United States",
+        "followers": 232000000
+    },
+    {
+        "name": "Beyoncé",
+        "description": "Singer, performer, cultural icon",
+        "country": "United States",
+        "followers": 310000000
+    },
+    {
+        "name": "Selena Gomez",
+        "description": "Singer, actress, producer",
+        "country": "United States",
+        "followers": 417000000
+    },
+    {
+        "name": "Taylor Swift",
+        "description": "Singer-songwriter, record breaker",
+        "country": "United States",
+        "followers": 361000000
+    },
+    {
+        "name": "Justin Bieber",
+        "description": "Singer, pop artist",
+        "country": "Canada",
+        "followers": 294000000
+    },
+    {
+        "name": "Rihanna",
+        "description": "Singer, entrepreneur, fashion icon",
+        "country": "Barbados / United States",
+        "followers": 332000000
+    },
+    {
+        "name": "Lionel Messi",
+        "description": "Footballer, many-time Ballon d'Or winner",
+        "country": "Argentina",
+        "followers": 506000000
+    },
+    {
+        "name": "Cristiano Ronaldo",
+        "description": "Footballer, global sports icon",
+        "country": "Portugal",
+        "followers": 664000000
+    },
+    {
+        "name": "Kim Kardashian",
+        "description": "Media personality, businesswoman",
+        "country": "United States",
+        "followers": 355000000
+    },
+    {
+        "name": "Dwayne Johnson",
+        "description": "Actor, producer, former wrestler (‘The Rock’)",
+        "country": "United States",
+        "followers": 392000000
+    },
+    {
+        "name": "Kylie Jenner",
+        "description": "Media personality, entrepreneur, beauty mogul",
+        "country": "United States",
+        "followers": 392000000
+    },
+    {
+        "name": "BTS – V (Kim Taehyung)",
+        "description": "Musician, member of BTS",
+        "country": "South Korea",
+        "followers": 68000000
+    }
+]
